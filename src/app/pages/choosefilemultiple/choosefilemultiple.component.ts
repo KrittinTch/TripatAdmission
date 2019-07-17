d4b3815a2d36607fe2c09ef3a1ce292b7971c376
nichanutnicha <aum_nutnicha40@hotmail.com>
2019-07-17 15:03:39 +0700

    Choosefilemultiple

--- src/app/pages/choosefilemultiple/choosefilemultiple.component.ts
+++ src/app/pages/choosefilemultiple/choosefilemultiple.component.ts
@@ -1,14 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-choosefilemultiple',
-  templateUrl: './choosefilemultiple.component.html',
-  styleUrls: ['./choosefilemultiple.component.scss']
+  selector: "app-choosefilemultiple",
+  templateUrl: "./choosefilemultiple.component.html",
+  styleUrls: ["./choosefilemultiple.component.scss"]
 })
 export class ChoosefilemultipleComponent implements OnInit {
-
+  filemultiple = [];
   constructor() { }
 
+detectfile2(ev) {
+  var files: Array<any> = ev.target.files;
+  console.log(files.length);
+  if (files) {
+    Array.from(files).forEach((files: any) => {
+      console.log(files.name);
+      this.filemultiple.push(files);
+
+    });
+  }
+}
   ngOnInit() {
   }
 
